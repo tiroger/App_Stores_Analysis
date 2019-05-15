859241657e73f0a83ffcf99b3593b47fdcfe8012
ROG LEF <tirogerl@gmail.com>
2019-05-15 13:17:35 -0400

    Cleaning up files

--- Data/row_deleter.py
+++
@@ -1,7 +0,0 @@
-import csv
-with open('AppleStore.csv', 'r') as inp, open('AppleStore_clean.csv', 'w') as out:
-    writer = csv.writer(out)
-    for row in csv.reader(inp):
-        if row[1] != "NA":
-            writer.writerow(row)
-
